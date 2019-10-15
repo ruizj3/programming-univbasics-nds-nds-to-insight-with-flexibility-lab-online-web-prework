dcd22584cba72aa2ef6df83f80b969e479cb874c
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-15 01:18:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
-
+pp directors_database
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
 
@@ -87,3 +87,5 @@ def studios_totals(nds)
   movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
   return gross_per_studio(movies_with_director_names)
 end
+
+
